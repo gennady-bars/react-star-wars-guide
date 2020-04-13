834138e8e367be8e1bc1720e83eb54d2152ce23c
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2020-04-13 12:41:02 +0300

    Update app.js

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -17,7 +17,7 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import StarshipDetails from '../sw-components/starship-details';
 
 export default class App extends Component {
